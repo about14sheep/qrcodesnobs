bfc4e2459cbaa96b077f015a7218bf85262bf9ec
about14sheep <aburger91@gmail.com>
2022-05-02 22:06:00 -0500

    UI updates

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ import { Router } from "https://deno.land/x/oak/mod.ts";
 import { qrcode } from "https://deno.land/x/qrcode/mod.ts";
 
 export const router = new Router();
-router.get("/", (ctx) => {
-    ctx.response.body = "qrcode generator with no bs";
+router.get("/", async (ctx) => {
+    await ctx.send({
+        root: `${Deno.cwd()}/static`,
+        index: "index.html"
+    });
 });
 router.post("/", async (ctx) => {
     const body = ctx.request.body();
