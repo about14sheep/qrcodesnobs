6d4c3d737e741cbbb0a6ec9ae9d59362f12eb46d
about14sheep <aburger91@gmail.com>
2022-05-04 18:55:09 -0500

    updates html page

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,12 @@ export const router = new Router();
 router.get("/", async (ctx) => {
     await ctx.send({
         root: `${Deno.cwd()}/static`,
-        index: "index.html"
+        index: 'index.html'
     });
 });
 router.post("/", async (ctx) => {
     const body = ctx.request.body();
     const val = await body.value;
-    const url = val?.url;
-    const code = await qrcode(url);
-    ctx.response.body = code;
+    const code = await qrcode(val);
+    ctx.response.body = {code};
 });
